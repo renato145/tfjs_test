f4b96c9691cb8868c83639c7c8f24011134705af
Renato Hermoza <renato.hermoza@pucp.edu.pe>
2020-02-28 00:00:51 +1030

    set layout img

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React, { useState } from 'react';
-import * as tf from '@tensorflow/tfjs';
+// import * as tf from '@tensorflow/tfjs';
 import './App.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, InputGroup, FormControl, Button, Image } from 'react-bootstrap';
+
+const exampleImage = 'https://cdn.glitch.com/5bf7c54b-c36f-4009-a191-186909fb788e%2Fcat_flickr_publicdomain.jpg'
 
 const App = () => {
-  const tensor = tf.randomNormal([10]);
-  const [ showData, setShowData ] = useState();
-  tensor.array().then(d => {
-    console.log(d);
-    setShowData(d[0]);
-  })
+  const [ imgUrl, setImgUrl ] = useState(exampleImage);
+  const [ imgCurrent, setImgCurrent ] = useState(exampleImage);
+  const loadImage = () => {
+    setImgCurrent(imgUrl);
+  };
 
   return (
     <Container>
       <Row>
-        <Col>{showData}</Col>
+        <Col>
+          <h1 className='mt-4 mb-4'>tfjs test</h1>
+          <InputGroup className='w-75 mb-3'>
+            <FormControl
+              value={imgUrl}
+              onChange={e => setImgUrl(e.target.value)}
+              placeholder='Image url'
+              aria-label='Image url'
+            />
+            <InputGroup.Append>
+              <Button variant='outline-secondary' onClick={loadImage}>Load image</Button>
+            </InputGroup.Append>
+          </InputGroup>
+          <Image className='w-75' src={imgCurrent} />
+        </Col>
       </Row>
     </Container>
   );
