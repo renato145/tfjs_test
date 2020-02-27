4ad58713dabdedd2e6d486f031089adb11ee4640
Renato Hermoza <renato.hermoza@pucp.edu.pe>
2020-02-28 00:42:58 +1030

    Updates

---
+++ precache-manifest.531b1c5ace4226aba627725929d78d35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6df12c1dd429c60e227aba500d1d77",
+    "url": "/tfjs_test/index.html"
+  },
+  {
+    "revision": "c1790af302b46d1ce19f",
+    "url": "/tfjs_test/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "08ea21d05db36a8396f9",
+    "url": "/tfjs_test/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c1790af302b46d1ce19f",
+    "url": "/tfjs_test/static/js/2.ef96382f.chunk.js"
+  },
+  {
+    "revision": "c8c16ab852dffff36e55d1a0289a34ca",
+    "url": "/tfjs_test/static/js/2.ef96382f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ea21d05db36a8396f9",
+    "url": "/tfjs_test/static/js/main.8173afca.chunk.js"
+  },
+  {
+    "revision": "10d1cd05e6fa822826a5",
+    "url": "/tfjs_test/static/js/runtime-main.8e558dff.js"
+  }
+]);
