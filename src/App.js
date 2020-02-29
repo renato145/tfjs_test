3987a00790c7391f2a53323bc7187f1ff2a18716
Renato <renato145@hotmail.com>
2020-02-29 14:11:25 +1030

    add heatmap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React, { useState, useEffect, useRef } from 'react';
 import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as tf from '@tensorflow/tfjs';
 import './App.css';
 import {
   Container,
@@ -14,11 +15,14 @@ import {
   Spinner,
 } from 'react-bootstrap';
 
+const IMAGE_SIZE = 224;
+
 const exampleImage =
   'https://cdn.glitch.com/5bf7c54b-c36f-4009-a191-186909fb788e%2Fcat_flickr_publicdomain.jpg';
 
 const App = () => {
   const imgRef = useRef();
+  const hmRef = useRef();
   const uploadImageRef = useRef();
   const [imgUrl, setImgUrl] = useState(exampleImage);
   const [imgCurrent, setImgCurrent] = useState(exampleImage);
@@ -29,6 +33,9 @@ const App = () => {
     mobilenet.load().then(model => {
       setModel(model);
       console.log('model loaded');
+      console.log(model);
+      console.log(imgRef.current);
+      testModel(model);
     });
   }, []);
 
@@ -53,6 +60,31 @@ const App = () => {
       const res = pred.map(d => `${d.className} (${d.probability.toFixed(2)})`);
       setPredictions(res);
     });
+  };
+
+  const testModel = model => {
+    const res = tf.tidy(() => {
+      const img = tf.browser.fromPixels(imgRef.current);
+      const normalized = img.toFloat().mul(model.normalizationConstant).add(model.inputMin);
+      let resized = normalized;
+      const originalShape = img.shape;
+      if (img.shape[0] !== IMAGE_SIZE || img.shape[1] !== IMAGE_SIZE) {
+          const alignCorners = true;
+          resized = tf.image.resizeBilinear(normalized, [IMAGE_SIZE, IMAGE_SIZE], alignCorners);
+      }
+      const batched = resized.reshape([-1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+      // See layers:
+      // model.model.artifacts.modelTopology.node.filter(d => d.name.includes('pointwise/Relu6')).forEach(({ name }) => {
+      //   const shape = model.model.execute(batched, name).shape;
+      //   console.log(`${name}: ${shape}`);
+      // });
+      let heatmap = model.model.execute(batched, 'module_apply_default/MobilenetV1/MobilenetV1/Conv2d_13_pointwise/Relu6').mean([0, -1]);
+      heatmap = heatmap.sub(heatmap.min())
+      heatmap = heatmap.div(heatmap.max())
+      return heatmap.expandDims(-1).resizeBilinear(originalShape.slice(0,2), true);
+    });
+    tf.browser.toPixels(res, hmRef.current);
+    console.log(res)
   };
 
   return (
@@ -98,12 +130,16 @@ const App = () => {
           </InputGroup.Append>
         </InputGroup>
         <Row className="justify-content-center">
-          <Col md={8}>
+          <Col lg={9}>
             <Image
               crossOrigin="anonymous"
               className="w-100 mb-2"
               ref={imgRef}
               src={imgCurrent}
+            />
+            <canvas
+              className="w-100 mb-2"
+              ref={hmRef}
             />
             <div className="mb-2 ml-2">
               <Button
