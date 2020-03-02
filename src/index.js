533798279560ec3763ff0aa3c0f8ec3e721b56f6
Renato Hermoza <renato.hermoza@pucp.edu.pe>
2020-03-03 00:48:37 +1030

    test slider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ImageHeatmap from './ImageHeatmap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ImageHeatmap />, document.getElementById('root'));
