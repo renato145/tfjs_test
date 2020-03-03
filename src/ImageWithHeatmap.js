4d5f5c474381336350fb2d51727fa7cb9eff4437
Renato <renato145@hotmail.com>
2020-03-04 10:00:07 +1030

    format

--- src/ImageWithHeatmap.js
+++ src/ImageWithHeatmap.js
@@ -3,8 +3,8 @@ import ReactCompareImage from 'react-compare-image';
 import { Image } from 'react-bootstrap';
 
 const ImageWithHeatmap = forwardRef(
-  ({ imgURL, heatmapURL, className }, ref) => {
-    return heatmapURL ? (
+  ({ imgURL, heatmapURL, className }, ref) => ( 
+    heatmapURL ? (
       <ReactCompareImage
         leftImage={imgURL}
         leftImageCss={{ position: 'static' }}
@@ -18,8 +18,8 @@ const ImageWithHeatmap = forwardRef(
         ref={ref}
         src={imgURL}
       />
-    );
-  }
+    )
+  )
 );
 
 export default ImageWithHeatmap;
